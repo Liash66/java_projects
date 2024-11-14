71b47bc8b8e7acd49f309acf4bc31583ce135e48
Andrey Koshelev <AKoshelev2004@yandex.ru>
2024-11-14 22:13:16 +0200

    update and add checkmate

--- lab_3/src/Figures/Bishop.java
+++ lab_3/src/Figures/Bishop.java
@@ -7,24 +7,9 @@ public class Bishop extends Figure {
 
     @Override
     public boolean canMove(int row, int col, int row1, int col1) {
-//        if (!super.canMove(row, col, row1, col1)){
-//            return false;
-//        }
-				for (int i = 0; i < 7; i++) {
-					if (((row+i==row1) && (col+i==col1)) || ((row-i==row1) && (col+i==col1)) || ((row+i==row1) && (col-i==col1)) || ((row-i==row1) && (col-i==col1))){
-						return true;
-					}		
-				}
-        return false;
-    }
-
-    @Override
-    public boolean canAttack(int row, int col, int row1, int col1) {
-				for (int i = 0; i < 7; i++) {
-					if (((row+i==row1) && (col+i==col1)) || ((row-i==row1) && (col+i==col1)) || ((row+i==row1) && (col-i==col1)) || ((row-i==row1) && (col-i==col1))){
-						return true;
-					}		
-				}
+        if (Math.abs(row - row1) == Math.abs(col - col1)) {
+            return true;
+        }
         return false;
     }
 }
